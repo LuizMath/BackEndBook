7b7cebacbdb10cc8c5ffd5714c4e0fd75181e56b
Luiz Matheus <luizmatheus@MacBook-Air-de-Luiz.local>
2026-05-09 23:50:01 -0300

    Proteção contra ataques

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import fastifySensible from "@fastify/sensible";
+import rateLimit from "@fastify/rate-limit";
+import type {} from "@fastify/rate-limit";
 import fastify from "fastify";
 import userRoutes from "./routes/user.routes";
 import bookRoutes from "./routes/book.routes";
@@ -12,6 +14,8 @@ app.register(cors, {
 app.register(fastifySensible);
 app.register(userRoutes);
 app.register(bookRoutes);
+
+await app.register(rateLimit, { global: false });
 
 app.setErrorHandler((error: any, req, reply) => {
   const statusCode = error.statusCode ?? 500;
@@ -31,3 +35,29 @@ app.setErrorHandler((error: any, req, reply) => {
       });
   }
 });
+
+app.setNotFoundHandler(
+  {
+    preHandler: app.rateLimit({
+      max: 3,
+      timeWindow: 500,
+    }),
+  },
+  (request, reply) => {
+    request.log.warn(
+      {
+        request: {
+          method: request.method,
+          url: request.url,
+          query: request.query,
+          params: request.params,
+        },
+      },
+      "Resource not found"
+    );
+
+    reply.code(404);
+
+    return { message: "Not Found" };
+  }
+);
